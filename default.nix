5759f0d08be5e20c98e09f72fef921d411e765dc
Steven Taschuk <steven@amotlpaa.org>
2016-09-28 21:32:11 -0400

    fdfd: Stash fetches & facts in S3.

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 let nixpkgs = import <nixpkgs> {};
 in { awk ? nixpkgs.gawk
+   , aws ? nixpkgs.aws
    , bash ? nixpkgs.bash
    , bc ? nixpkgs.bc
    , coreutils ? nixpkgs.coreutils
@@ -16,6 +17,7 @@ in { awk ? nixpkgs.gawk
      name = "fdfd-0.1.0";
      src = ./.;
      inherit awk
+             aws
              bash
              bc
              coreutils
